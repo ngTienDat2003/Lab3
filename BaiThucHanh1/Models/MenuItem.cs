e136c97ac4495d519fb6ba968244e492885dda7d
unknown <ngtiendat61003@gmail.com>
2023-09-20 16:55:07 +0700

    Lab 2

---
+++ BaiThucHanh1/Models/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiThucHanh1.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+    
+}
